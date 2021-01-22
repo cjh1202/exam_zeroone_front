269a8ff0b56c834bfb57b56166a56db22bddc9e2
cjh1202 <1395501780@qq.com>
2021-01-22 15:34:09 +0800

    题库模块查询所有试题功能,删除，添加试题功能完善

--- bootstrap-table/js/btTable.js
+++ bootstrap-table/js/btTable.js
@@ -9,61 +9,39 @@ function reLoad(){
     $("#table1").bootstrapTable("refresh");
 }
 
-function load(){
-    var jsondata={};
+function load() {
+    var jsondata = {};
     var item = localStorage.getItem("user");
     var user = JSON.parse(item);
 
 
     $("#table1").bootstrapTable({
-        url:"http://localhost:8080/exam_zeroone_ssm/findAllQuestionBank",
+        url: "http://localhost:8080/exam_zeroone_ssm/findAllQuestionBank",
 
-        striped:true,//是否显示隔行换色
-        pageNumber:1,//初始化加载第一页
-        pagination:true,//是否分页
-        sidePagination:'server',
-        pageSize:2,//每页记录数
-        method:'POST',
+        striped: true,//是否显示隔行换色
+        pageNumber: 1,//初始化加载第一页
+        pagination: true,//是否分页
+        sidePagination: 'server',
+        pageSize: 2,//每页记录数
+        method: 'POST',
         queryParams: function (params) {
-            var paramsJSON ={
-                offset:params.offset,
-                pageNumber:params.limit,
-                u_id:user.u_id
+            var paramsJSON = {
+                offset: params.offset,
+                pageNumber: params.limit,
+                u_id: user.u_id
             };
 
             return JSON.stringify(paramsJSON)
         },
 
-/*
-        method:"POST",
-        dataType:"JSON",
-        striped : true, //是否显示行间隔色
-        pageNumber : 1, //初始化加载第一页
-        pagination : true,//是否分页
-        singleSelect:false,//是否可多选
-        sidePagination : "server",//server:服务器端分页|client：前端分页
-        pageSize : 10,//单页记录数  告知前端使用者 每页显示多少个
-        // showRefresh : true,//刷新按钮
-        //查询时携带的参数  data：JSON.stringify()
-        queryParams: function (params) {
-            var paramsJSON ={};
-            paramsJSON.u_id=user.u_id;
-
-            var temp = {
-                offset :params.offset,// SQL语句起始索引
-                pageNumber : params.limit  // 每页显示数量
-            };
-            return JSON.stringify(paramsJSON,temp);
-
-        },*/
-        columns:[
+        columns: [
             {
-                title:'行号',
-                align:"center",//水平居中
-                halign:"center",//垂直居中
+                title: '行号',
+                align: "center",//水平居中
+                halign: "center",//垂直居中
                 width: 100,
-                formatter:function(value,row,index){
-                    return index+1;
+                formatter: function (value, row, index) {
+                    return index + 1;
                 }
 
             },
@@ -72,50 +50,82 @@ function load(){
             //     field:'id'
             // },
             {
-                title:'题目类型',
-                align:"center",//水平居中
-                halign:"center",//垂直居中
-                field:'q_type',
+                title: '题目类型',
+                align: "center",//水平居中
+                halign: "center",//垂直居中
+                field: 'q_type',
                 width: 100
             },
             {
-                title:'试题分类',
-                align:"center",//水平居中
-                halign:"center",//垂直居中
-                field:'q_classify',
+                title: '试题分类',
+                align: "center",//水平居中
+                halign: "center",//垂直居中
+                field: 'q_classify',
                 width: 100
             },
             {
-                title:'试题内容',
-                align:"center",//水平居中
-                halign:"center",//垂直居中
-                field:'q_content',
+                title: '试题内容',
+                align: "center",//水平居中
+                halign: "center",//垂直居中
+                field: 'q_content',
 
             },
             {
-                title:'添加时间',
-                align:"center",//水平居中
-                halign:"center",//垂直居中
-                field:'q_createTime',
+                title: '添加时间',
+                align: "center",//水平居中
+                halign: "center",//垂直居中
+                field: 'q_createTime',
                 width: 100
             }
-             ,
+            ,
             {
-                title:'操作',
+                title: '操作',
                 // field:'createTime', //和后台pojo属性进行对应
-                align:'center',//列名称、列数据居中 水平居中
-                halign:'center', //垂直居中
-                width:'150px',//设置列宽
-                formatter:function(value,row,index){
+                align: 'center',//列名称、列数据居中 水平居中
+                halign: 'center', //垂直居中
+                width: '150px',//设置列宽
+                formatter: function (value, row, index) {
                     //如果将来 涉及到字符串数据传入参数  需要设置单引号
-                    let d='<a href="javascript:void(0);" onclick="removeData(\''+row.id+'\')">删除</a>'
-                    let m='<a href="javascript:void(0);" onclick="modifyGoods(\''+row.id+'\',\''
-                        +row.detail+'\',\''+row.name+'\',\''+row.category+'\',\''+row.stock+'\',\''+row.money+'\')">修改</a>'
-                    let detail='<a href="javascript:void(0);" onclick="fetchDetail(\''+row.id+'\')">详情</a>'
-                    return d+" "+m+" "+detail
+                    let d = '<a href="javascript:void(0);" onclick="removeData(\'' + row.q_id + '\')">删除</a>'
+                    let m = '<a href="javascript:void(0);" onclick="modifyGoods(\'' + row.id + '\',\''
+                        + row.detail + '\',\'' + row.name + '\',\'' + row.category + '\',\'' + row.stock + '\',\'' + row.money + '\')">修改</a>'
+                    let detail = '<a href="javascript:void(0);" onclick="fetchDetail(\'' + row.id + '\')">详情</a>'
+                    return d + " " + m + " " + detail
                 }
             }
-         ]
+        ]
     })
+
+
 }
 
+function removeData(q_id) {
+    console.log(q_id)
+    if (q_id) {
+        let url = "http://localhost:8080/exam_zeroone_ssm/deleteTopic"
+        var dataJSON = {};
+
+        dataJSON.q_id = q_id;
+        $.ajax({
+            url: url,
+            data: dataJSON,
+            dataType: 'json',
+            type: 'POST',
+            success: function (result) {
+                console.log(result.mark)
+                //将返回的result数据，渲染到页面上
+                if (result.mark == 1) {
+                    //删除成功-->重新渲染表格的数据
+                    reLoad()
+                } else {
+                    alert("删除失败");
+                }
+            },
+            error: function (result) {
+            }
+        })
+
+    } else {
+        alert("数据有问题！无法删除");
+    }
+}
