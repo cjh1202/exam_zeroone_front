e7b110363f8042a87a5363a0011c57f7323eab36
root <root@com>
2021-01-22 17:55:04 +0800

    完成题库的前端

--- bootstrap-table/js/btTable.js
+++ bootstrap-table/js/btTable.js
@@ -87,8 +87,9 @@ function load() {
                 formatter: function (value, row, index) {
                     //如果将来 涉及到字符串数据传入参数  需要设置单引号
                     let d = '<a href="javascript:void(0);" onclick="removeData(\'' + row.q_id + '\')">删除</a>'
-                    let m = '<a href="javascript:void(0);" onclick="modifyGoods(\'' + row.id + '\',\''
-                        + row.detail + '\',\'' + row.name + '\',\'' + row.category + '\',\'' + row.stock + '\',\'' + row.money + '\')">修改</a>'
+                    let m='<a href="javascript:void(0);" onclick="modifyTopic(\''+row.q_id+'\',\''
+                        +row.q_type+'\',\''+row.q_classify+'\',\''+row.q_content+'\',\''+row.q_a+'\',\''
+                        +row.q_b+'\',\''+row.q_c+'\',\''+row.q_d+'\',\''+row.q_answer+'\',\''+row.q_score+'\')">修改</a>'
                     let detail = '<a href="javascript:void(0);" onclick="fetchDetail(\'' + row.id + '\')">详情</a>'
                     return d + " " + m + " " + detail
                 }
@@ -106,6 +107,7 @@ function removeData(q_id) {
         var dataJSON = {};
 
         dataJSON.q_id = q_id;
+        console.log(dataJSON)
         $.ajax({
             url: url,
             data: dataJSON,
@@ -128,4 +130,66 @@ function removeData(q_id) {
     } else {
         alert("数据有问题！无法删除");
     }
+}
+
+function modifyTopic(q_id,q_type,q_classify,q_content,q_a,q_b,q_c,q_d,q_answer,q_score) {
+
+
+   if(q_type=="单选题"){
+       localStorage.setItem("q_id",q_id);
+       layer.open({
+           type:2,//可传入的值有：0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）。
+           title:'试题修改',
+           maxmin:false,
+           shadeClose:false,
+           area:[1000+'px',($(window).height() - 50)+'px'],//弹出层的宽高
+           content:'../main/updateSingleChoice.html',//设置弹出层打开的页面
+           //弹出层页面成功打开后，的设置
+           success:function(layero,index){
+               //当前是表格页面     修改是表格的子页面   父页面JS代码中将数据传递给子页面中
+               //获取子页面HTML对象
+               let childBody= layer.getChildFrame('body',index);
+
+               //在childBody子页面body区域中find（查找）input标签name属性是xxx的那个input对象，给其设置值为xxx
+               $(childBody).find('textarea[id=content]').val(q_content);
+               $(childBody).find('input[id=answerA]').val(q_a);
+               $(childBody).find('input[id=answerB]').val(q_b);
+               $(childBody).find('input[id=answerC]').val(q_c);
+               $(childBody).find('input[id=answerD]').val(q_d);
+               $(childBody).find('input[name=answer][value='+ q_answer +']').checked="checked";
+               $(childBody).find('input[id=score]').val(q_score);
+               $(childBody).find('input[id=classify]').val(q_classify);
+               //获取子页面JS对象
+           }
+       });
+
+
+   }else {
+       localStorage.setItem("q_id",q_id);
+       layer.open({
+           type:2,//可传入的值有：0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）。
+           title:'试题修改',
+           maxmin:false,
+           shadeClose:false,
+           area:[1000+'px',($(window).height() - 50)+'px'],//弹出层的宽高
+           content:'../main/updateBriefAnswer.html',//设置弹出层打开的页面
+           //弹出层页面成功打开后，的设置
+           success:function(layero,index){
+               //当前是表格页面     修改是表格的子页面   父页面JS代码中将数据传递给子页面中
+               //获取子页面HTML对象
+               let childBody= layer.getChildFrame('body',index);
+
+               //在childBody子页面body区域中find（查找）input标签name属性是xxx的那个input对象，给其设置值为xxx
+               $(childBody).find('textarea[id=content1]').val(q_content);
+               $(childBody).find('textarea[id=answer1]').val(q_answer);
+               $(childBody).find('input[id=score1]').val(q_score);
+               $(childBody).find('input[id=classify1]').val(q_classify);
+               //获取子页面JS对象
+           }
+       });
+
+   }
+
+
+
 }
